455518c2c1f370ce8fdb59bb00fb8e696b7e9a9d
piyush-saurabh <saurabh.piyush2807@gmail.com>
2021-05-08 20:16:18 +0530

    added endpoint for toggling onboard led

--- web-server/components/server/server.c
+++ web-server/components/server/server.c
@@ -2,10 +2,21 @@
 #include <string.h>
 #include <esp_log.h>
 #include <esp_http_server.h>
+#include <cJSON.h>
+#include <driver/gpio.h>
 
 #define TAG "SERVER"
 
-// Handler Function
+// onboard LED PIN Number
+#define LED 2
+
+// Initialize the GPIO
+void InitializeLed(){
+    gpio_pad_select_gpio(LED);
+    gpio_set_direction(LED,GPIO_MODE_OUTPUT);
+}
+
+// 1st Handler Function (GET handler)
 static esp_err_t on_url_hit(httpd_req_t *req)
 {
     ESP_LOGI(TAG, "url %s was hit", req->uri);
@@ -14,9 +25,56 @@ static esp_err_t on_url_hit(httpd_req_t *req)
     return ESP_OK;
 }
 
+// 2nd Handler Function (GET handler)
+static esp_err_t on_get_temperature(httpd_req_t *req)
+{
+    ESP_LOGI(TAG, "url %s was hit", req->uri);
+    char *message = "{\"temperature\": \"25\"}";
+    httpd_resp_send(req, message, strlen(message));
+    return ESP_OK;
+}
+
+// 3rd Handler Function (POST handler)
+static esp_err_t on_led_set(httpd_req_t *req)
+{
+    ESP_LOGI(TAG, "url %s was hit", req->uri);
+    
+    // Create a buffer to hold the infomation posted in the request
+    char buf[150];
+    memset(buf, 0, sizeof(buf)); //initialize the buffer with zero
+
+    // Get the information received in the request and store it in buffer
+    // TODO validate content_len
+    httpd_req_recv(req, buf, req->content_len);
+
+    // Parse the request coming from the request
+    /*
+        Sample Request Body
+        {"isLedOn": false}
+    */
+    cJSON *payload = cJSON_Parse(buf);
+    cJSON *isLedOn = cJSON_GetObjectItem(payload, "isLedOn");
+
+    // Toggle the GPIO Pin
+    gpio_set_level(LED,cJSON_IsTrue(isLedOn));
+
+    // Delete the JSON object to clean up the memory
+    cJSON_Delete(payload);
+
+    // Send back the response (acknowledgement)
+    httpd_resp_set_status(req, "204 NO CONTENT");
+
+    // Send the null response
+    httpd_resp_send(req, NULL, 0);
+    return ESP_OK;
+}
+
 // This function is called from main()
 void RegisterEndPoints(void)
 {
+    // Initialize the GPIO (for controlling onboard LED)
+    InitializeLed();
+
     // Create an endpoint for HTTP request
     // 1. Create a handler for the server
     // 2. Create the config
@@ -27,6 +85,9 @@ void RegisterEndPoints(void)
     // Config for the server. The macro will start the server with default configuration
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
 
+    // Create wildcard route. This will point to a callback (already implemented by ESP IDF)
+    config.uri_match_fn = httpd_uri_match_wildcard;
+
     // Start the server
     ESP_LOGI(TAG, "starting server");
     if (httpd_start(&server, &config) != ESP_OK)
@@ -34,21 +95,34 @@ void RegisterEndPoints(void)
         ESP_LOGE(TAG, "COULD NOT START SERVER");
     }
 
-    // Register the endpoint
+    // Routing works in sequence
+
+    // Register 2nd endpoint
+    httpd_uri_t second_end_point_config = {
+        .uri = "/api/temperature",
+        .method = HTTP_GET,
+        .handler = on_get_temperature // handler callback function once this URL is hit
+    };
+    // Register the handler with the server
+    httpd_register_uri_handler(server, &second_end_point_config);
+
+    // Register 3rd endpoint
+    httpd_uri_t third_end_point_config = {
+        .uri = "/api/led",
+        .method = HTTP_POST,
+        .handler = on_led_set // handler callback function once this URL is hit
+    };
+    // Register the handler with the server
+    httpd_register_uri_handler(server, &third_end_point_config);
+
+    // Register 1st endpoint
+    // wild card route should be at the end
     httpd_uri_t first_end_point_config = {
-        .uri = "/",
+        .uri = "/*",
         .method = HTTP_GET,
         .handler = on_url_hit // handler callback function once this URL is hit
     };
-
     // Register the handler with the server
     httpd_register_uri_handler(server, &first_end_point_config);
-
-
-
-
-
-
-
 
 }
