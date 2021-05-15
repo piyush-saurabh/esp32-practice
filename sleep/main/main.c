50b14172d38d75d307b6a298f6055df49f5f5e87
piyush-saurabh <saurabh.piyush2807@gmail.com>
2021-05-15 16:01:17 +0530

    added light sleep with gpio

--- sleep/main/main.c
+++ sleep/main/main.c
@@ -8,6 +8,10 @@
 #include <esp_sleep.h> // For activating sleep functions
 #include <esp_log.h>
 #include <esp32/rom/uart.h> // Used to include uart_tx_wait_idle()
+#include <driver/rtc_io.h> // This allows to use GPIO pins while sleeping  
+
+// Use Boot button to wake up from light sleep
+#define INPUT_PIN 0
 
 // Light sleep with timer
 void light_sleep_timer()
@@ -34,8 +38,56 @@ void light_sleep_timer()
     printf("Total sleep duration is %lld ms\n", (after - before) / 1000);
 }
 
-void light_sleep_gpio()
+// Wake up from light sleep by clicking a button
+void light_sleep_timer_gpio()
 {
+    // Configure the GPIO pin as input (button)
+    gpio_pad_select_gpio(INPUT_PIN);
+    gpio_set_direction(INPUT_PIN, GPIO_MODE_INPUT);
+
+    // Setup the wakeup interrupt (not for wakeup)
+    // This will be triggered when button is released
+    gpio_wakeup_enable(INPUT_PIN, GPIO_INTR_LOW_LEVEL);
+
+    // Enable the GPIO
+    esp_sleep_enable_gpio_wakeup();
+
+    // Configure light sleep for 5 sec
+    esp_sleep_enable_timer_wakeup(5000000);
+
+    // Loop so that the program the process of sleep and wakeup continues
+    while (true)
+    {
+        // Halt the application untill the button is released
+        // rtc_gpio_get_level() has the capability to read the pins during sleep
+        if (rtc_gpio_get_level(INPUT_PIN) == 0)
+        {
+            printf("Please release the button\n");
+            do
+            {
+                // wait for 10ms
+                vTaskDelay(pdMS_TO_TICKS(10));
+            } while (rtc_gpio_get_level(INPUT_PIN) == 0);
+        }
+
+        printf("Starting light sleep\n");
+        uart_tx_wait_idle(CONFIG_ESP_CONSOLE_UART_NUM);
+
+        int64_t before = esp_timer_get_time();
+
+        esp_light_sleep_start();
+
+        int64_t after = esp_timer_get_time();
+
+        // Cause of wakeup
+        // It can be button press or timer
+        esp_sleep_wakeup_cause_t reason = esp_sleep_get_wakeup_cause();
+
+        printf("Light Sleep duration is for %lld ms, reason was %s\n", (after - before) / 1000,
+               reason == ESP_SLEEP_WAKEUP_TIMER ? "timer" : "button");
+    }
+
+
 
 }
 
@@ -56,5 +108,9 @@ void deep_sleep_ext1()
 
 void app_main(void)
 {
-    light_sleep_timer();
+    // demo for light sleep only with timer
+    //light_sleep_timer();
+
+    // demo for light sleep with gpio and timer
+    light_sleep_timer_gpio();
 }
