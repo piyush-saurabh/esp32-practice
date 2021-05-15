b255a16fcab1fe732369fa6b4a3a3eebb18fff81
piyush-saurabh <saurabh.piyush2807@gmail.com>
2021-05-15 16:41:27 +0530

    added deep sleep

--- sleep/main/main.c
+++ sleep/main/main.c
@@ -10,8 +10,20 @@
 #include <esp32/rom/uart.h> // Used to include uart_tx_wait_idle()
 #include <driver/rtc_io.h> // This allows to use GPIO pins while sleeping  
 
-// Use Boot button to wake up from light sleep
+// Use Boot button (0) or any RTC GPIO to wake up from light sleep (keep the button in pullup state)
 #define INPUT_PIN 0
+
+// Dummy 3.3 V source
+#define DUMMY_VCC_PIN 23
+
+ // HACK
+ // Use GPIO as 3.3V source (since there is only 1 3.3V pin on ESP32 board)
+void set_dummy_voltage_source()
+{
+    gpio_pad_select_gpio(DUMMY_VCC_PIN);
+    gpio_set_direction(DUMMY_VCC_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_level(DUMMY_VCC_PIN, 1);
+}
 
 // Light sleep with timer
 void light_sleep_timer()
@@ -38,6 +50,9 @@ void light_sleep_timer()
     printf("Total sleep duration is %lld ms\n", (after - before) / 1000);
 }
 
+
+
+
 // Wake up from light sleep by clicking a button
 void light_sleep_timer_gpio()
 {
@@ -47,7 +62,9 @@ void light_sleep_timer_gpio()
 
     // Setup the wakeup interrupt (not for wakeup)
     // This will be triggered when button is released
-    gpio_wakeup_enable(INPUT_PIN, GPIO_INTR_LOW_LEVEL);
+    // Keep the button in pullup state
+    gpio_wakeup_enable(INPUT_PIN, GPIO_INTR_LOW_LEVEL); // default button state: ON (pullup)
+    // gpio_wakeup_enable(INPUT_PIN, GPIO_INTR_HIGH_LEVEL); // default button state: OFF (pull down)
 
     // Enable the GPIO
     esp_sleep_enable_gpio_wakeup();
@@ -60,14 +77,16 @@ void light_sleep_timer_gpio()
     {
         // Halt the application untill the button is released
         // rtc_gpio_get_level() has the capability to read the pins during sleep
-        if (rtc_gpio_get_level(INPUT_PIN) == 0)
+        // Button default state: ON (pull up)
+        if (rtc_gpio_get_level(INPUT_PIN) == 0) // use 0 if default button state is ON (pullup)
         {
             printf("Please release the button\n");
             do
             {
                 // wait for 10ms
                 vTaskDelay(pdMS_TO_TICKS(10));
-            } while (rtc_gpio_get_level(INPUT_PIN) == 0);
+            } while (rtc_gpio_get_level(INPUT_PIN) == 0); // use 0 if default button state is ON (pullup)
+
         }
 
         printf("Starting light sleep\n");
@@ -91,9 +110,25 @@ void light_sleep_timer_gpio()
 
 }
 
+
+
+// record information during deep sleep
+// This compiler directive instructs to write data to the special part of the memory so that the value is persisted even after reset from deep sleep
+RTC_DATA_ATTR int timesWokenUp = 0; 
+
+// very simple deep sleep with timer
 void deep_sleep_timer()
 {
+    // Configure the wakeup time in micro second
+    esp_sleep_enable_timer_wakeup(5 * 1000000); // sleep for 5 sec
 
+    printf("Going to deep sleep. woken up %d\n", timesWokenUp++);
+
+    // Go to deep sleep
+    // After coming back from deep sleep, chip resets
+    esp_deep_sleep_start();
+
+    // Code here will not be executed (unlike light sleep)
 }
 
 void deep_sleep_ext0()
@@ -108,9 +143,14 @@ void deep_sleep_ext1()
 
 void app_main(void)
 {
+    // Dummy voltage source
+    //set_dummy_voltage_source();
+
     // demo for light sleep only with timer
     //light_sleep_timer();
 
     // demo for light sleep with gpio and timer
-    light_sleep_timer_gpio();
+    // light_sleep_timer_gpio();
+
+    deep_sleep_timer();
 }
