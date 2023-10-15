d594ef06b83f868ca92c92824c9684de2727b4f5
Piyush Saurabh <saurabh.piyush2807@gmail.com>
2023-10-15 21:23:30 +0530

    added dht11 sensor code

---
+++ sensors/README.md
@@ -0,0 +1,9 @@
+## Temperature Sensor
+Sensor used: DHT11
+
+![image info](./images/dht11.png)
+
+
+Ref
+- https://esp-idf-lib.readthedocs.io/en/latest/groups/dht.html
+- https://github.com/UncleRus/esp-idf-lib/tree/master/components/dht
--- sensors/components/temperature_sensor/temperature_sensor.c
+++ sensors/components/temperature_sensor/temperature_sensor.c
@@ -5,17 +5,36 @@
 #include <esp_log.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include "dht.h"
 
 
 #define TAG "TEMPERATURE_SENSOR"
 
+#define SENSOR_PIN 33
+
 void temperature_sensor(void *param)
 {
+    int16_t humidity;
+    int16_t temperature;
+    gpio_num_t sensorPin = 33;
+
     while(1)
     {
-        printf("Calculating the temperature...\n");
+        printf("Reading the temperature...\n");
 
-        // wait for 1sec
+        esp_err_t error = dht_read_data(DHT_TYPE_DHT11, sensorPin, &humidity, &temperature);
+
+        if(error == ESP_OK)
+        {
+            // Output is integer
+            // E.g. humidity=625 is 62.5 %, temperature=244 is 24.4 degrees Celsius
+            humidity = humidity / 10;
+            temperature = temperature / 10;
+
+            printf("Temperature = %d °C\nHumidity = %d %%\n\n", temperature, humidity);
+        }
+
+        // wait for 5 sec
         vTaskDelay(5000 / portTICK_PERIOD_MS);
     }
 }
